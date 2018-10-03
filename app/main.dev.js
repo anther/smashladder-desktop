7ffb277e4461ec91dc501518b9c42a1ea91fe9c3
Anther <anthrzy@gmail.com>
2018-10-03 12:13:32 -0400

    "Restore" window when launching from tray to prevent blank window from opening?

--- app/main.dev.js
+++ app/main.dev.js
@@ -10,14 +10,21 @@
  *
  * @flow
  */
-import { app, BrowserWindow, shell, ipcMain, Menu, Tray, nativeImage } from 'electron';
+import {
+  app,
+  BrowserWindow,
+  shell,
+  ipcMain,
+  Menu,
+  Tray,
+  nativeImage
+} from 'electron';
 import { autoUpdater } from 'electron-updater';
 import _ from 'lodash';
 import AutoLaunch from 'auto-launch';
 import electronSettings from 'electron-settings';
-import path from "path";
+import path from 'path';
 import Files from './utils/Files';
-
 
 let mainWindow = null;
 
@@ -96,35 +103,30 @@ if (isSecondInstance) {
   app.quit();
 }
 
-
 let tray = null;
 let debugTray = { send: () => {} };
 app.on('ready', async () => {
-	const imagePath = Files.createApplicationPath('./external/android-icon-36x36.png');
-	const trayImage = nativeImage.createFromPath(imagePath);
-	ipcMain.on('debugTray', (event) => {
-		debugTray = event.sender;
-		event.sender.send('debugTray', imagePath);
-	});
+  const imagePath = Files.createApplicationPath(
+    './external/android-icon-36x36.png'
+  );
+  const trayImage = nativeImage.createFromPath(imagePath);
+  ipcMain.on('debugTray', event => {
+    debugTray = event.sender;
+    event.sender.send('debugTray', imagePath);
+  });
 
+  tray = new Tray(trayImage);
+  updateTray();
 
-	tray = new Tray(trayImage);
-	updateTray();
-
-	tray.on('click', () => {
-		if(mainWindow.isMinimized())
-		{
-			mainWindow.show();
-		}
-		else
-		{
-			mainWindow.show();
-		}
-	});
-	tray.on('double-click', () => {
-		mainWindow.show();
-	});
-	tray.setToolTip('SmashLadder Dolphin Launcher');
+  tray.on('click', () => {
+    mainWindow.show();
+    mainWindow.restore();
+    mainWindow.focus();
+  });
+  tray.on('double-click', () => {
+    mainWindow.show();
+  });
+  tray.setToolTip('SmashLadder Dolphin Launcher');
 });
 app.on('ready', async () => {
   if (
@@ -148,9 +150,9 @@ app.on('ready', async () => {
     shell.openExternal(url);
   });
 
-  mainWindow.on('minimize', (event) => {
-	  event.preventDefault();
-	  mainWindow.hide();
+  mainWindow.on('minimize', event => {
+    event.preventDefault();
+    mainWindow.hide();
   });
 
   mainWindow.webContents.on('did-finish-load', () => {
@@ -158,105 +160,97 @@ app.on('ready', async () => {
       throw new Error('"mainWindow" is not defined');
     }
     const autoLaunched = (process.argv || []).indexOf('--hidden') !== -1;
-	  if (process.env.START_MINIMIZED) {
+    if (process.env.START_MINIMIZED) {
       mainWindow.minimize();
-    } else if(!autoLaunched){
+    } else if (!autoLaunched) {
       mainWindow.show();
       mainWindow.focus();
-    } if(autoLaunched){
+    }
+    if (autoLaunched) {
       mainWindow.hide();
       mainWindow.minimize();
     }
-
   });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
-
 });
 
-
 const setupAutoLaunch = async () => {
-	const autoLaunch = new AutoLaunch({
-		name: 'SmashLadder Dolphin Launcher',
-		path: app.getPath('exe'),
-		isHidden: true,
-	});
-	let launchAtStartup = await electronSettings.get(LAUNCH_AT_STARTUP_KEY);
-	if(launchAtStartup === undefined)
-	{
-		try{
-			const isEnabled = await autoLaunch.isEnabled();
-			if(!isEnabled)
-			{
-				autoLaunch.enable();
-				launchAtStartup = true;
-				electronSettings.set(LAUNCH_AT_STARTUP_KEY, launchAtStartup);
-			}
-		}
-		catch (error) {
-			console.error(error);
-		}
-	}
-	return {
-		autoLaunch,
-		launchAtStartup,
-	};
+  const autoLaunch = new AutoLaunch({
+    name: 'SmashLadder Dolphin Launcher',
+    path: app.getPath('exe'),
+    isHidden: true
+  });
+  let launchAtStartup = await electronSettings.get(LAUNCH_AT_STARTUP_KEY);
+  if (launchAtStartup === undefined) {
+    try {
+      const isEnabled = await autoLaunch.isEnabled();
+      if (!isEnabled) {
+        autoLaunch.enable();
+        launchAtStartup = true;
+        electronSettings.set(LAUNCH_AT_STARTUP_KEY, launchAtStartup);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  return {
+    autoLaunch,
+    launchAtStartup
+  };
 };
 
 const updateTray = async () => {
-	const { autoLaunch, launchAtStartup } = await setupAutoLaunch();
-	const contextMenu = Menu.buildFromTemplate([
-		{
-			label: app.getVersion(),
-			enabled: false,
-		},
-		{
-			type: 'separator',
-		},
-		{
-			label: 'Launch At Startup',
-			checked: !!launchAtStartup,
-			type: 'checkbox',
-			click: () => {
-				debugTray.send('debugTray', `launch at startup ${launchAtStartup}`);
-				if(launchAtStartup)
-				{
-					autoLaunch.disable()
-						.then(()=>{
-							electronSettings.set(LAUNCH_AT_STARTUP_KEY, false);
-							updateTray();
-						})
-						.catch((error)=>{
-							if(debugTray)
-							{
-								debugTray.send('debugTray', error);
-							}
-						});
-				}
-				else
-				{
-					autoLaunch.enable()
-						.then(()=>{
-							electronSettings.set(LAUNCH_AT_STARTUP_KEY, true);
-							updateTray();
-						})
-						.catch((error)=>{
-							if(debugTray)
-							{
-								debugTray.send('debugTray', error);
-							}
-						});
-				}
-			}
-		},
-		{
-			label: 'Quit',
-			click:  () => {
-				app.quit();
-			}
-		}
-	]);
-	tray.setContextMenu(contextMenu);
+  const { autoLaunch, launchAtStartup } = await setupAutoLaunch();
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: app.getVersion(),
+      enabled: false
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Launch At Startup',
+      checked: !!launchAtStartup,
+      type: 'checkbox',
+      click: () => {
+        debugTray.send('debugTray', `launch at startup ${launchAtStartup}`);
+        if (launchAtStartup) {
+          autoLaunch
+            .disable()
+            .then(() => {
+              electronSettings.set(LAUNCH_AT_STARTUP_KEY, false);
+              updateTray();
+            })
+            .catch(error => {
+              if (debugTray) {
+                debugTray.send('debugTray', error);
+              }
+            });
+        } else {
+          autoLaunch
+            .enable()
+            .then(() => {
+              electronSettings.set(LAUNCH_AT_STARTUP_KEY, true);
+              updateTray();
+            })
+            .catch(error => {
+              if (debugTray) {
+                debugTray.send('debugTray', error);
+              }
+            });
+        }
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
 };
